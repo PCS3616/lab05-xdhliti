a405be71d80c70c2437b3e8576e2061e15d34b60
Guilherme Jacule <123499899+xdhliti@users.noreply.github.com>
2025-02-10 22:38:04 -0300

    Add files via upload

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,12 +1,12 @@
 @ /0000
-LD N ; ac <= n  000
 ; JZ /0f00 ; termina o programa se n = 0 || 002
 SC /0300 ; 004
 HM /0a00 ; 006
 
 
 @ /0300
-; JP /004 
+; JP /004
+LD N ; ac <= n  000 
 JZ /0400 
 MM AUX ; i = ac = 4 || i = AC = 3 || i = ac = 2 
 ML RES ; AC = AC*RES = 4 || AC = 3*4 || AC = 4*3*2
