a9453dbd3ff8c143d3ed985d8188c2dd28f12330
Guilherme Jacule <123499899+xdhliti@users.noreply.github.com>
2025-02-10 21:38:43 -0300

    Add files via upload

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -8,24 +8,22 @@ HM /a00 ; 006
 N K /004
 RES K /001
 CTE K /001
-fat K /001
 it K /001
 
 
 @ /300
 ;JP /004 
 MM it ; i = ac = 4 || i = AC = 3 || i = ac = 2 
-ML fat ; AC = AC*RES = 4 || AC = 3*4 || AC = 4*3*2
-MM fat ; RES <= AC || RES = 4 || res = 12 || 24 
+ML RES ; AC = AC*RES = 4 || AC = 3*4 || AC = 4*3*2
+MM RES ; RES <= AC || RES = 4 || res = 12 || 24 
 LD it ; AC = i = 4 || ac = 3 || ac = 2
+MM RES 
 SB CTE ; AC = AC - 1 = 3 || ac = 2 || ac = 1
 JZ /400 
 JP /300
 
 @ /400
-LD fat
-MM RES
-RS /300
+  RS /300
 
 @ /f00 ; f00
-HM /a00
+  HM /a00
