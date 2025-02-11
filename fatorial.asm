67889f5ab2692f202cee81def97c68d4f89e74ea
Guilherme Jacule <123499899+xdhliti@users.noreply.github.com>
2025-02-10 23:14:02 -0300

    Add files via upload

--- fatorial.asm
+++ fatorial.asm
@@ -1,28 +1,31 @@
 @ /0000
-LD N ; ac <= n  000
-JZ /0f00 ; termina o programa se n = 0 || 002
-SC /0010 ; 004
+SC /0200 ; 004
+LD FAT
+MM RES ; res <= fat 006
 HM /0a00 ; 006
-
-
-@ /0010
-; JP /004 
-MM AUX ; i = ac = 4 || i = AC = 3 || i = ac = 2 
-ML RES ; AC = AC*RES = 4 || AC = 3*4 || AC = 4*3*2
-MM RES ; RES <= AC || RES = 4 || res = 12 || 24 
-LD AUX ; AC = i = 4 || ac = 3 || ac = 2
-SB CTE ; AC = AC - 1 = 3 || ac = 2 || ac = 1
-JZ /0400 
-JP /0300
-
-@ /0400
-RS /0300
-
-@ /0f00 ; f00
-HM /0a00
 
 @ /0100
 N K /0004
 RES K /0001
 AUX K /0001
+FAT K /0001
 CTE K /0001
+
+@ /0200
+LD N ; ac <= n  000
+JZ /020e
+MM AUX ; i = ac = 4 || i = AC = 3 || i = ac = 2 
+ML FAT ; AC = AC*RES = 4 || AC = 3*4 || AC = 4*3*2
+MM FAT ; RES <= AC || RES = 4 || res = 12 || 24 
+LD AUX ; AC = i = 4 || ac = 3 || ac = 2
+SB CTE ; AC = AC - 1 = 3 || ac = 2 || ac = 1
+JP /0200
+RS /0200 ; 030e
+
+; @ /0400
+; RS /0300
+
+@ /0f00 ; f00
+HM /0a00
+
+
