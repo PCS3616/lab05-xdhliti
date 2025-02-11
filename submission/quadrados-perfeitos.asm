74f71847490fbcbbe3222b1d5892acb4d9fb9b8e
Guilherme Jacule <123499899+xdhliti@users.noreply.github.com>
2025-02-11 17:05:34 -0300

    Add files via upload

--- submission/quadrados-perfeitos.asm
+++ submission/quadrados-perfeitos.asm
@@ -1,40 +1,47 @@
 @ /000
-LD base
-MM ptr
-LD square
-MM [ptr]
-SC /200
-HM /004
+SC /200 ; vai pra subrot
+HM /004 ; termina o programa
 
-@ /200 
+
+
+@ /200
 JP /000
-LD i 
-ML two
-AD one
-AD square
-MM square 
-MM [ptr]
-LD ptr 
+LD mem
 AD two
-MM ptr
-LD i
-SB n
-JZ /400
+AD x ; AC = 9<mem> (concateno a mem com a instru equivalente a gaurdar MM mem)
+MM /500
+LD i ; AC = i 
+ML two ; AC = 2*AC
+AD one ; AC += 1
+AD aux ; AC = AC + aux
+MM aux ; aux = AC = 2i + 1
 LD i 
-AD one
-MM i
-JP /202
+AD one ; AC = (AC = 1) + 1
+MM i ; i = AC
+SB n ; AC = AC - n = AC - 63
+JZ /400 ; se i == n = 63
+JP /202 ; volta para o inicio do for 
 
 @ /400
 RS /200
 
+@ /500
+k /0000
+
+@ /100
+k /000
+
 
 @ /300
-n k =64
-i k =0
-two k =2
-one k =1
-final k =64
-square k =0
+i k /000
+aux k /000
+aux2 k /000
+mem k /100 ; ponteiro que comeca em 0100
+one k =1 ;pra somar 
+two k =2 ;pra multiplicar
+n k =63
 base k /0100
-ptr k =0
+x k /9000
+
+
+
