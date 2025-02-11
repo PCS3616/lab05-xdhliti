8992f1451fcc46f26855b43a49e8701f64a5472c
Guilherme Jacule <123499899+xdhliti@users.noreply.github.com>
2025-02-10 23:33:55 -0300

    Add files via upload

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,5 +1,5 @@
-START SC /200 ; 004
-END HM =0 ; 006
+SC /200 ; 004
+HM =0 ; 006
 
 @ /100
 N K =4
@@ -9,17 +9,16 @@ FAT K =1
 CTE K =1
 
 @ /200
-JP START
 LD N ; ac <= n  000
-JZ /212
+JZ /210
 MM AUX ; i = ac = 4 || i = AC = 3 || i = ac = 2 
 ML FAT ; AC = AC*RES = 4 || AC = 3*4 || AC = 4*3*2
 MM FAT ; RES <= AC || RES = 4 || res = 12 || 24 
 LD AUX ; AC = i = 4 || ac = 3 || ac = 2
 SB CTE ; AC = AC - 1 = 3 || ac = 2 || ac = 1
-JP /204
+JP /202
 LD FAT
 MM RES ; res <= fat 006
-RS /200 ; 030e
+RS /200 
 
 
